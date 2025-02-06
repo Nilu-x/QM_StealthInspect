8324f9daf6f26055dd4c9e0f624cedb77cea09d2
Josiah <Josiah@LAPTOP-MVB6DTUM>
2025-02-06 15:04:08 -0500

    Add project files.

---
+++ QM_StealthInspect/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MGSC;
+using HarmonyLib;
+using System.Reflection;
+
+namespace QM_StealthInspect
+{
+    public static class Plugin
+    {
+        public static string ModAssemblyName => Assembly.GetExecutingAssembly().GetName().Name;
+
+        [Hook(ModHookType.AfterConfigsLoaded)]
+        public static void AfterConfig(IModContext context)
+        {
+            new Harmony("Nilux_" + ModAssemblyName).PatchAll();
+        }
+    }
+}
