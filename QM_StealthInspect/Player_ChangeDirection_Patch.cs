254220c466eae37054b770bc284f5d617f30c720
Josiah Vorce <josiah.vorce@gmail.com>
2025-02-06 18:58:40 -0500

    english

--- QM_StealthInspect/Player_ChangeDirection_Patch.cs
+++ QM_StealthInspect/Player_ChangeDirection_Patch.cs
@@ -17,12 +17,14 @@ namespace QM_StealthInspect
         The behavior which causes the player to have to stand up after inspecting a creature while sneaking is actually tied to changing view direction.
         Inspecting happens with right click, which also turns the player.
 
-        The devs use player.anyactionprocessedflag for cases where they want this behavior, so far it was observed in 2 places.
-            1) when a sneaking player turns their camera
+        The devs use player.anyactionprocessedflag for cases where they want this behavior. For example:
+            1) when a sneaking player changes their facing direction
             2) when a sneaking player changes to another weapon
 
         It's clearly intentional that they want this, so keep in mind it may have balance implications. Maybe your unfair run will be slightly less so.
         I like to be able to recon without the tedium of standing up before inspecting every enemy in a room, but of course it's personal preference.
+
+        This mod should only change what happens when the player is sneaking and changes direction/inspects a creature.        
         */
 
         public static void Postfix(Player __instance) => __instance.AnyActionProcessedFlag = false;
