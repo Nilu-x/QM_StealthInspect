8b0ecc3bcf14b8838924acd755ab794b65a7d08b
Josiah Vorce <josiah.vorce@gmail.com>
2025-02-20 22:44:47 -0500

    Fixed exploit.

--- QM_StealthInspect/Player_ChangeDirection_Patch.cs
+++ QM_StealthInspect/Player_ChangeDirection_Patch.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using HarmonyLib;
 using MGSC;
 using UnityEngine;
@@ -13,21 +8,17 @@ namespace QM_StealthInspect
     [HarmonyPatch(typeof(Player), nameof(Player.ChangeDirection))]
     public static class PlayerChangeDirectionPatch
     {
-        /*
-        The behavior which causes the player to have to stand up after inspecting a creature while sneaking is actually tied to changing view direction.
-        Inspecting happens with right click, which also turns the player.
-
-        The devs use player.anyactionprocessedflag for cases where they want this behavior. For example:
-            1) when a sneaking player changes their facing direction
-            2) when a sneaking player changes to another weapon
-
-        It's clearly intentional that they want this, so keep in mind it may have balance implications. Maybe your unfair run will be slightly less so.
-        I like to be able to recon without the tedium of standing up before inspecting every enemy in a room, but of course it's personal preference.
-
-        This mod should only change what happens when the player is sneaking and changes direction/inspects a creature.        
-        */
-
-        public static void Postfix(Player __instance) => __instance.AnyActionProcessedFlag = false;
-
+        public static void Prefix(Player __instance, ref bool markActionFlag)
+        {
+            // If we don't want to trigger AP penalties, force markActionFlag to false
+            if (__instance.FreeInventoryUse)
+            {
+                Debug.Log("[QM_StealthInspect] Preventing AP penalty by forcing markActionFlag to false.");
+                markActionFlag = false;  // This prevents the game from setting AnyActionProcessedFlag to true
+            }
+        }
     }
 }
+
+
+
